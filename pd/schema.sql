7c7d99d7d83e61135ce580222896ccad65106afd
disksing <i@disksing.com>
2018-11-30 12:13:40 +0800

    update schema

--- pd/schema.sql
+++ pd/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE `pd_stores` IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS `pd_stores` (
         `id` BIGINT,
-        `address` VARCHAR(32),
-        `state` VARCHAR(32),
-        `version` VARCHAR(32),
+        `address` VARCHAR(255),
+        `state` VARCHAR(255),
+        `version` VARCHAR(255),
         `capacity` BIGINT,
         `available` BIGINT,
         `leader_count` BIGINT,
         `region_count` BIGINT,
         `is_busy` INT,
-        `start_ts` VARCHAR(32),
-        `last_heartbeat_ts` VARCHAR(32),
-        `uptime` VARCHAR(32),
+        `start_ts` VARCHAR(255),
+        `last_heartbeat_ts` VARCHAR(255),
+        `uptime` VARCHAR(255)
 );
